e9966eeb887f63668986d4df896fc62ca484171b
shoxa <sllx.ok@gmail.com>
2022-09-02 00:07:09 +0500

    exported greeting changed to default

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}`);
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
 };
- 

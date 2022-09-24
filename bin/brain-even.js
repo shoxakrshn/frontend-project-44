18b4a28418d1a6fb4304fe98b5de96bf6dce99d7
shoxa <sllx.ok@gmail.com>
2022-09-24 17:28:03 +0500

    refactor brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,61 +2,43 @@
 import readlineSync from 'readline-sync';
 
 //  Generating Random Number
-const getRandomInt = (min = 1, max = 20) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomInt = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
 
 //  get user name
-const getName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
+const getName = () => readlineSync.question('May I have your name? ');
 
 //  check whether the number is Even or no
-const isEven = (value) => {
-  if (value % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
+const checkEven = (value) => {
+  const result = value % 2 === 0;
+  return result ? 'yes' : 'no';
 };
 
 //  get answer from user
-const getAnswer = () => {
-  const answer = readlineSync.question('Your answer: ');
-  return answer;
-};
+const getAnswer = () => readlineSync.question('Your answer: ');
 
 //  check answer for even
-const checkAnswer = (random, userAnswer) => isEven(random) === userAnswer;
+const checkAnswer = (random, userAnswer) => checkEven(random) === userAnswer;
 
 //  Game begining
 console.log('Welcome to the Brain Games!');
 const name = getName();
+console.log(`Hello, ${name}!`);
 
 console.log('"yes" if the number is even, otherwise answer "no".');
 
 // implementing game logic
-let i = 0;
-
-while (i < 3) {
+for (let i = 0; i < 3; i += 1) {
   const randomValue = getRandomInt();
   console.log(`Question: ${randomValue}`);
   const answer = getAnswer();
-
-  if (answer !== 'yes' && answer !== 'no') {
-    console.log('Error type of answer');
-    break;
-  }
-
   const result = checkAnswer(randomValue, answer);
-
   if (result) {
     console.log('Correct!');
-    i += 1;
-    if (i === 3) {
+    if (i === 2) {
       console.log(`Congratulations, ${name}!`);
     }
   } else {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkEven(randomValue)}'`);
     console.log(`Let's try again, ${name}!`);
     break;
   }

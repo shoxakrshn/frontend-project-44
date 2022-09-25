dc0eb325379f7a28ca260e9c05b530980d71d3d7
shoxa <sllx.ok@gmail.com>
2022-09-25 18:56:36 +0500

    adde required files for brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+//import runGame from '../games/gcd.js';
+
+//runGame();
+console.log('hello, gcd');
---
+++ games/gcd.js
@@ -0,0 +1,9 @@
+import randomNumber from '../src/random-number-generator.js';
+import {
+  greeting,
+  getAnswer,
+  getName,
+  checkAnswer,
+  congratulate,
+  failed,
+} from '../src/index.js';
